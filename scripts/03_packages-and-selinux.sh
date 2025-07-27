3b916e641504dbc4cce565e4ac9a0f0c84d78566
Yamato <lab8010@gmail.com>
2025-07-27 09:45:33 +0900

    Fix some packaged dependency

--- scripts/03_packages-and-selinux.sh
+++ scripts/03_packages-and-selinux.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -e
 
-# SELinuxをpermissiveに変更
+# 基本パッケージのインストール
+PACKAGES=(
+  git
+  vim-enhanced
+  wget
+  net-tools
+  bash-completion
+  lvm2
+  chrony
+  python3.11
+  python3.11-devel
+)
+dnf install -y "${PACKAGES[@]}"
+
+# Python3.11 をデフォルトに設定
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+alternatives --set python3 /usr/bin/python3.11
+
+# pip 再インストール（安全のため）
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+
+# SELinux を無効化
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 setenforce 0
-sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
-
-# Firewalldの停止（学習用）
-systemctl disable --now firewalld
-
-# 必要パッケージの導入
-dnf install -y git vim wget net-tools bash-completion lvm2 chrony
-
-# chrony起動と設定
-systemctl enable --now chronyd
-sed -i '/^server /d' /etc/chrony.conf
-echo 'server ntp.nict.jp iburst' >> /etc/chrony.conf
-systemctl restart chronyd
 
 echo "パッケージとSELinux設定を完了しました"
